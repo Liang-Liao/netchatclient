adcb06b8d524a4bf62ead783af65be91d9fca29d
syso <1005252998@qq.com>
2018-07-20 01:19:59 +0800

    我的实验类修改

--- src/main/java/netchat/netchatserver/App.java
+++ src/main/java/netchat/netchatserver/App.java
@@ -4,10 +4,9 @@ package netchat.netchatserver;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("我的实验类");
+		System.out.println("Hello World!");
+	}
 }
