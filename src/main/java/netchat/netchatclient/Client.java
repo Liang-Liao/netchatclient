e977e2095ecf566cdd0dcaa5e1b11a6a46bd8537
ll <1005252998@qq.com>
2018-07-20 13:49:35 +0800

    添加了ZeroMQ客户端示例代码

---
+++ src/main/java/netchat/netchatclient/Client.java
@@ -0,0 +1,33 @@
+package netchat.netchatclient;
+
+import org.zeromq.ZMQ;
+
+/**
+ * Hello world!
+ *  客户端实验类
+ */
+public class Client {
+	public static void main(String[] args) {
+		ZMQ.Context context = ZMQ.context(1);
+
+        //  Socket to talk to server
+        System.out.println("Connecting to hello world server…");
+
+        ZMQ.Socket requester = context.socket(ZMQ.REQ);
+        requester.connect("tcp://localhost:5555");
+
+        for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
+            String request = "Hello";
+            System.out.println("Sending Hello " + requestNbr);
+            requester.send(request.getBytes(), 0);
+
+            byte[] reply = requester.recv(0);
+            System.out.println("Received " + new String(reply) + " " + requestNbr);
+        }
+        requester.close();
+        context.term();
+		
+		System.out.println("我的实验类");
+		System.out.println("Hello World!");
+	}
+}
